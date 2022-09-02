2251b335af74c738aab36dfdfebee0f0ea593ed5
chethan <chethan.18sherugar@gmail.com>
2022-09-02 18:15:53 +0530

    chore:created class

--- connecter.js
+++ connecter.js
@@ -1,85 +1,85 @@
-import { localStorageHandler, setvalueToObject } from './storage.js';
-import { prepareTask, insertTaskIntoDOM, domElements, deleteTaskInUI, deleteTaskInObject, updateTaskStatus } from './app.js';
+// function previous(){import { localStorageHandler, setvalueToObject } from './storage.js';
+// import { prepareTask, insertTaskIntoDOM, domElements, deleteTaskInUI, deleteTaskInObject, updateTaskStatus } from './app.js';
 
 
-export let newTasksObj = {};
+// export let newTasksObj = {};
 
-export const getObjectIdValue = {
-    getNewTask() {
-        return domElements.getnewText.value;
-    },
-    getTaskId() {
-        const id = getObjectKeysAsArray(newTasksObj);
-        return id[id.length - 1];
-    }
-};
+// export const getObjectIdValue = {
+//     getNewTask() {
+//         return domElements.getnewText.value;
+//     },
+//     getTaskId() {
+//         const id = getObjectKeysAsArray(newTasksObj);
+//         return id[id.length - 1];
+//     }
+// };
 
-function getObjectKeysAsArray(obj) {
-    console.log(obj)
-    if (Object.keys(obj)) {
-        return Object.keys(obj);
-    }
-}
-
-
-export function prepareObject() {
-    setvalueToObject.prepareObjectForLocal(getObjectIdValue.getNewTask(), newTasksObj);
-
-}
-
-export function addNewTaskOnUi() {
-    const task = getObjectIdValue.getNewTask();
-    const id = getObjectIdValue.getTaskId();
-    const taskElement = prepareTask(task, id, false);
-    insertTaskIntoDOM(taskElement);
-}
-
-export function addTaskOnStorage() {
-    localStorageHandler.setData('user1', newTasksObj)
-}
-
-export function getDataFromDB() {
-    newTasksObj = JSON.parse(localStorageHandler.getData('user1'));
-}
-
-export function addDbTaskOnUI() {
-    const idArray = getObjectKeysAsArray(newTasksObj);
-    loadDbTaskOnUI(newTasksObj, idArray);
-}
-
-function loadDbTaskOnUI(object, arr) {
-    arr.forEach(key => {
-        let obj = object[key];
-        const taskElement = prepareTask(obj.task, obj.id, obj.status)
-        insertTaskIntoDOM(taskElement);
-    })
-}
-
-export function updateTaskStatusHandler(e) {
-    const id = getElementId.getParentelementID(e);
-    updateTaskStatus(newTasksObj, id);
-    localStorageHandler.setData(newTasksObj);
-
-}
+// function getObjectKeysAsArray(obj) {
+//     console.log(obj)
+//     if (Object.keys(obj)) {
+//         return Object.keys(obj);
+//     }
+// }
 
 
+// export function prepareObject() {
+//     setvalueToObject.prepareObjectForLocal(getObjectIdValue.getNewTask(), newTasksObj);
 
-export function deleteTaskHandler(e) {
-    const id = getElementId.getParentelementID(e);
-    const domElement = element.getElement(id);
-    deleteTaskInUI(domElement);
-    deleteTaskInObject(newTasksObj, id);
-    localStorageHandler.setData(newTasksObj);
-}
+// }
 
-const element = {
-    getElement(id) {
-        return document.getElementById(id)
-    }
-}
+// export function addNewTaskOnUi() {
+//     const task = getObjectIdValue.getNewTask();
+//     const id = getObjectIdValue.getTaskId();
+//     const taskElement = prepareTask(task, id, false);
+//     insertTaskIntoDOM(taskElement);
+// }
 
-const getElementId = {
-    getParentelementID(e) {
-        return e.target.parentNode.id;
-    }
-}
+// export function addTaskOnStorage() {
+//     localStorageHandler.setData('user1', newTasksObj)
+// }
+
+// export function getDataFromDB() {
+//     newTasksObj = JSON.parse(localStorageHandler.getData('user1'));
+// }
+
+// export function addDbTaskOnUI() {
+//     const idArray = getObjectKeysAsArray(newTasksObj);
+//     loadDbTaskOnUI(newTasksObj, idArray);
+// }
+
+// function loadDbTaskOnUI(object, arr) {
+//     arr.forEach(key => {
+//         let obj = object[key];
+//         const taskElement = prepareTask(obj.task, obj.id, obj.status)
+//         insertTaskIntoDOM(taskElement);
+//     })
+// }
+
+// export function updateTaskStatusHandler(e) {
+//     const id = getElementId.getParentelementID(e);
+//     updateTaskStatus(newTasksObj, id);
+//     localStorageHandler.setData(newTasksObj);
+
+// }
+
+
+
+// export function deleteTaskHandler(e) {
+//     const id = getElementId.getParentelementID(e);
+//     const domElement = element.getElement(id);
+//     deleteTaskInUI(domElement);
+//     deleteTaskInObject(newTasksObj, id);
+//     localStorageHandler.setData(newTasksObj);
+// }
+
+// const element = {
+//     getElement(id) {
+//         return document.getElementById(id)
+//     }
+// }
+
+// const getElementId = {
+//     getParentelementID(e) {
+//         return e.target.parentNode.id;
+//     }
+// }}
--- view.js
+++ view.js
@@ -1,32 +1,122 @@
-// (()=>{
-import { prepareObject, addTaskOnStorage, addNewTaskOnUi, getDataFromDB, addDbTaskOnUI } from './connecter.js'
-import { domElements } from './app.js'
+// function previous(){// (()=>{
+// import { prepareObject, addTaskOnStorage, addNewTaskOnUi, getDataFromDB, addDbTaskOnUI } from './connecter.js'
+// import { domElements } from './app.js'
 
-function init() {
-    registerEventHandlers();
+// function init() {
+//     registerEventHandlers();
+// }
+
+// function registerEventHandlers() {
+//     onLodeStoredTasks();
+//     buttonsEventHandel();
+// }
+
+// function onLodeStoredTasks() {
+//     getDataFromDB();
+//     addDbTaskOnUI();
+// }
+
+// function buttonsEventHandel() {
+
+//     domElements.getTextBtn.addEventListener('click', addTaskActions);
+//     // domElements.getTaskHistory.addEventListener('click', updateCounter);
+
+// }
+// function addTaskActions() {
+//     prepareObject();
+//     addTaskOnStorage();
+//     addNewTaskOnUi();
+// }
+
+// init();
+// }
+
+class ToDoList {
+    constructor() {
+        console.log(typeof this.domPropValue.newTask)
+        this.init();
+    }
+
+    app = document.querySelector('#app');
+    domPropValue = {
+        newTask: '.newTask',
+        taskList: '.taskList',
+        addBtn: '.addBtn',
+        totalTask: '.totalCount',
+        completedTask: '.completedCount',
+        pendingTask: '.pendingCount',
+
+        getAddBtnRef: () => {
+            return this.app.querySelector(this.domPropValue.addBtn);
+        },
+        getNewTaskRef: () => {
+            return this.app.querySelector(this.domPropValue.newTask);
+        },
+        getTaskListRef: () => {
+            return this.app.querySelector(this.domPropValue.taskList);
+        },
+        getTotalTaskRef: () => {
+            return this.app.querySelector(this.domPropValue.totalTask);
+        },
+        getCompletedTaskRef: () => {
+            return this.app.querySelector(this.domPropValue.completedTask);
+        },
+        getPendingTaskRef: () => {
+            return this.app.querySelector(this.domPropValue.pendingTask);
+        },
+    };
+    init() {
+        this.registerHandlers();
+    }
+    registerHandlers() {
+        this.addBtnEventHandler();
+    }
+
+    addBtnEventHandler() {
+        this.domPropValue.getAddBtnRef().addEventListener('click', this.addTaskToList);
+    }
+    addTaskToList() {
+        // const v=this.domPropValue.getNewTaskRef().value;
+        // const id= Date.now()
+        this.prepareTask(this.domPropValue.getNewTaskRef.value, Date.now(), false)
+        // this.domPropValue.getTaskListRef().innerHTML='hello !!';
+    }
+    insertTaskIntoDOM(node) {
+        domElements.getTaskLists.appendChild(node);
+    }
+
+    prepareTask(value, id, status) {
+        const task = document.createTextNode(value);
+        const liElement = createEleme('li', { 'class': 'task', 'id': id });
+        const taskDelBtn = createEleme('input', { 'type': 'button', 'class': 'taskDeleteBtn', 'value': 'delete', 'id': id / 1000 });
+        let taskcheckbox;
+        status ? taskcheckbox = createEleme('input', { 'type': 'checkbox', 'class': 'taskcheckbox', 'id': id / 100, 'checked': '' }) : '';
+        status ? '' : taskcheckbox = createEleme('input', { 'type': 'checkbox', 'class': 'taskcheckbox', 'id': id / 100 });
+        taskcheckbox.addEventListener('click', (e) => { updateTaskStatusHandler(e) });
+        taskDelBtn.addEventListener('click', (e) => { deleteTaskHandler(e) });
+        appendElements(liElement, task, taskcheckbox, taskDelBtn);
+        return liElement;
+    }
+
+    createEleme(value, addAtt) {
+        const element = document.createElement(value);//
+        return addAttributes(element, addAtt);//here have to check addAtt identifier is object
+    }
+
+    addAttributes(mainElement, setAtt) {
+        const key = Object.keys(setAtt);
+        const value = Object.values(setAtt);
+        for (let i = key.length - 1; i >= 0; i--) {
+            mainElement.setAttribute(`${key[i]}`, `${value[i]}`);
+        }
+        return mainElement;
+    }
+
+    appendElements(parentNode, ...appChild) {
+        for (let i = appChild.length - 1; i >= 0; i--) {
+            parentNode.appendChild(appChild[i]);
+        }
+    }
+
 }
-
-function registerEventHandlers() {
-    onLodeStoredTasks();
-    buttonsEventHandel();
-}
-
-function onLodeStoredTasks() {
-    getDataFromDB();
-    addDbTaskOnUI();
-}
-
-function buttonsEventHandel() {
-
-    domElements.getTextBtn.addEventListener('click', addTaskActions);
-    // domElements.getTaskHistory.addEventListener('click', updateCounter);
-
-}
-function addTaskActions() {
-    prepareObject();
-    addTaskOnStorage();
-    addNewTaskOnUi();
-}
-
-init();
-// })();
+new toDoList()
